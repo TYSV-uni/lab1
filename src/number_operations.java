5323830e11f9313e24752a81a81aafc4f50d6ba1
Sventsytskyi Tymofii <tim.from.ua@gmail.com>
2024-09-16 07:40:41 +0300

    BigDecimal and BigInteger compatability added

--- src/number_operations.java
+++ src/number_operations.java
@@ -1,11 +1,15 @@
 import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.util.*;
+
 
 public class number_operations
 {
     public static void main(String[] args)
     {
         ArrayList<Number> tlist = new ArrayList<>();
+
 
         Number[] nums = {10, 20.5, 30, 40.7, 50, 60.3, 70, 80.1, 90, 100.9};
         int num1 = 2;
@@ -14,7 +18,8 @@ public class number_operations
         long num4 = 204;
         double num5 = 12.0;
         byte num6 = 13;
-        BigDecimal num7 = new BigDecimal("12020200202");
+        BigDecimal num7 = new BigDecimal("120202002.00");
+        BigInteger num8 = new BigInteger("234657442364");
 
         Collections.addAll(tlist, nums);
         tlist.add(num1);
@@ -24,15 +29,14 @@ public class number_operations
         tlist.add(num5);
         tlist.add(num6);
         tlist.add(num7);
+        tlist.add(num8);
 
         System.out.println("\nWhole list: " + tlist + "\n");
 
         System.out.print("Numbers as integers: [");
         for (Number x : tlist)
         {
-            String value = String.valueOf(x);
-            if (String.valueOf(x).contains("."))
-                value = String.valueOf(Math.round(x.doubleValue()));
+            BigDecimal value = new BigDecimal(String.valueOf(x)).setScale(0, RoundingMode.HALF_UP);
 
             if (tlist.indexOf(x) != tlist.size() - 1)
                 System.out.print(value + ", ");
@@ -43,7 +47,8 @@ public class number_operations
         System.out.print("\n\nNumbers formatted with 2 decimals: [");
         for (Number x : tlist)
         {
-            String value = String.format("%.2f", x.doubleValue()).replace(",", ".");
+            BigDecimal value = new BigDecimal(String.valueOf(x)).setScale(2, RoundingMode.HALF_UP);
+
             if (tlist.indexOf(x) != tlist.size() - 1)
                 System.out.print(value + ", ");
             else
@@ -66,7 +71,7 @@ public class number_operations
         ArrayList<Number> integers = new ArrayList<>();
 
         for (Number x : tlist)
-            if (x.doubleValue() % 1 == 0)
+            if (!String.valueOf(new BigDecimal(String.valueOf(x)).stripTrailingZeros()).contains("."))
                 integers.add(x);
 
         System.out.println("\nAll integers in a separate list: " + integers);
